dbb301c1140382e74d236c984711e2c0d23eaa30
Takács Eszter Magdolna <takieszti91@gmail.com>
2024-03-26 10:12:22 +0100

    Trying to use systemd to start at boot

--- Vagrantfile
+++ Vagrantfile
@@ -18,6 +18,9 @@ Vagrant.configure("2") do |config|
 
     config.vm.provision "file", source: "Dockerfile", destination: "/home/vagrant/Dockerfile"
     config.vm.provision "shell", path: "scripts_vagrant/copy_app.sh"
+    #config.vm.provision "file", source: "scripts_vagrant/docker-node-app\@.service", destination: "/tmp/docker-node-app\@.service"
+    config.vm.provision "file", source: "scripts_vagrant/docker-node-app.service", destination: "/tmp/docker-node-app.service"
+    # config.vm.provision "shell", inline: "mv /tmp/docker-node-app\@.service /lib/systemd/system/docker-node-app\@.service"
     config.vm.provision "shell", path: "scripts_vagrant/nodejs_app_test.sh"
 
     # config.vm.network "forwarded_port", guest: 3000, host: 3000
--- scripts_vagrant/nodejs_app_test.sh
+++ scripts_vagrant/nodejs_app_test.sh
@@ -4,10 +4,28 @@
 # port=3000
 
 app_name=sample-node-api
-port=18000
+# port=18000
 
 cd /home/vagrant/$app_name
 # Only for check npm commands in case of error in Docker build
 # sudo apt-get install npm
 docker build -t $app_name .
-docker run -p $port:$port -d $app_name
+
+#docker run -p $port:$port -d $app_name
+#mv /tmp/docker-node-app\@.service /etc/systemd/system/docker-node-app\@.service
+#mv /tmp/docker-node-app\@.service /usr/lib/systemd/system/docker-node-app\@.service
+mv /tmp/docker-node-app.service /usr/lib/systemd/system/docker-node-app.service
+chown root:root /usr/lib/systemd/system/docker-node-app.service
+chmod 644 /usr/lib/systemd/system/docker-node-app.service
+
+# apt install policycoreutils
+# restorecon docker-node-app.service
+
+# ln -s /lib/systemd/system/docker-node-app.service /etc/systemd/system/docker-node-app.service
+#systemctl daemon-reload
+systemctl daemon-reexec
+
+#systemctl start docker-nodejs-app@$app_name
+#systemctl enable docker-nodejs-app@$app_name
+systemctl start docker-nodejs-app
+systemctl enable docker-nodejs-app
