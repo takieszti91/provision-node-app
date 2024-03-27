671d7d443beece1620193c9f266727f38960e288
Takács Eszter Magdolna <takieszti91@gmail.com>
2024-03-27 10:49:01 +0100

    Vagrant generate variables_secure.tf from AWS credentials for Terraform

--- scripts_vagrant/setup_acces_keys.sh
+++ scripts_vagrant/setup_acces_keys.sh
@@ -9,3 +9,15 @@ secret_access_key=$(tail -n 1 $access_key_file|sed 's|^.*,\(.*\)$|\1|')
 # export AWS_SECRET_KEY_ID=$secret_access_key
 awsv2 configure set aws_access_key_id $access_key_id
 awsv2 configure set aws_secret_access_key $secret_access_key
+
+terraform_secure_variables_file="/vagrant/terraform/variables_secure.tf"
+if [ ! -f $terraform_secure_variables_file ]
+then
+    account_id=$(awsv2 sts get-caller-identity|grep Account|sed "s|^[^0-9]*\([0-9]*\)[^0-9]*$|\1|")
+    cat << 'EOF' > $terraform_secure_variables_file
+variable "aws_account" {
+    type = string
+EOF
+    echo "    default = \"$account_id\"" >> $terraform_secure_variables_file
+    echo "}" >> $terraform_secure_variables_file
+fi
